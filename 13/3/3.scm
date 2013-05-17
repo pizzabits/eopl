0680129ddedf44397003773a53fce136e8ef9b38
Michael Ridner <michael.ridner@gmail.com>
2013-05-17 20:44:54 +0300

    further explanation about 3.b

--- 13/3/3.scm
+++ 13/3/3.scm
@@ -1,15 +1,27 @@
-; uses PROC language definitions
+;; uses PROC language definitions
 
-;3.a
+;; 3.a
 (run "let sum = proc(x)
                   proc(y) 
                     -(x,-(0,y))
                        in ((sum 3) 4)")
 
-;3.b
-; 4 + 4 + 4 = 12
+;; 3.b
+;; As we know, -(x, -(0,y)) <=> x+y
+;; With that in mind, lets observe the makemult definition:
+;; it gets a maker as a parameter, which will be called with the same method,
+;; to be able to call it again later and (using Currying technic) another parameter, x.
 
-;3.c
+;; Now we check if x is zero, if it is then we return zero.
+;; Otherwise, we actually sum up the result of maker(x-1, 4)
+;; So what will happen is that we'll call ourself (*call maker*, which gets makemult proc)
+;; from the bottom of the recursion with 0+4, and then 4+4, and thus further
+;; until the number x is zeroed.
+;; Having explained this, the program calls (times4 3) so the calculation will be:
+;; 4 + 4 + 4 = 12
+;; and the result is (of course) (num-val 12).
+
+;; 3.c
 (run "let mult = proc (maker)
                     proc (number)
                       proc (multiplier)
